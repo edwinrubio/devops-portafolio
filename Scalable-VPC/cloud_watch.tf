8ccdacdc84272a9a531245f889dda4d5b825b503
Edwin Rubio <edwin.rubioing@outlook.com>
2024-03-05 20:11:38 -0500

    fix: old bugs in my platform

--- Scalable-VPC/cloud_watch.tf
+++ Scalable-VPC/cloud_watch.tf
@@ -1,4 +1,4 @@
-resource "aws_cloudwatch_metric_alarm" "asg_state_change_alarm" {
+resource "aws_cloudwatch_metric_alarm" "asg_state_change_alarm_app" {
   alarm_name          = "ASGStateChangeAlarm"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = 1
@@ -8,19 +8,90 @@ resource "aws_cloudwatch_metric_alarm" "asg_state_change_alarm" {
   statistic           = "SampleCount"
   threshold           = 80
   alarm_description   = "ASG state change detected"
-  alarm_actions       = [aws_sns_topic.autoscaling_update.arn]
+  alarm_actions       = [aws_sns_topic.autoscaling_update_app.arn]
 
   dimensions = {
     AutoScalingGroupName = aws_security_group.app_security_group.name
   }
 }
 
-resource "aws_sns_topic" "autoscaling_update" {
+resource "aws_sns_topic" "autoscaling_update_app" {
   name = "autoscaling_update"
 }
 
-resource "aws_sns_topic_subscription" "email_subscription" {
-  topic_arn = aws_sns_topic.autoscaling_update.arn
+resource "aws_sns_topic_subscription" "email_subscription_app" {
+  topic_arn = aws_sns_topic.autoscaling_update_app.arn
   protocol  = "email"
   endpoint  = "correo_electronico@example.com"
 }
+
+resource "aws_cloudwatch_log_group" "vpc_bastion_group_log" {
+  name = "flow-bastion-logs"
+  retention_in_days = 7  # Cambia el período de retención según tus requisitos
+}
+
+resource "aws_flow_log" "bastion_flow_log" {
+  iam_role_arn    = aws_iam_role.logs_role.arn
+  log_destination = aws_cloudwatch_log_group.vpc_bastion_group_log.arn
+  traffic_type    = "ALL"
+  vpc_id          = module.vpc-bastion.vpc_id
+}
+
+resource "aws_flow_log" "app_flow_log" {
+  iam_role_arn    = aws_iam_role.logs_role.arn
+  log_destination = aws_cloudwatch_log_group.vpc_app_group_log.arn
+  traffic_type    = "ALL"
+  vpc_id          = module.vpc-app.vpc_id
+}
+
+
+
+resource "aws_cloudwatch_log_group" "vpc_app_group_log" {
+  name = "flow-app-logs"
+
+  retention_in_days = 7  # Cambia el período de retención según tus requisitos
+}
+
+
+# permission log group
+
+data "aws_iam_policy_document" "logs_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["vpc-flow-logs.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "logs_role" {
+  name               = "logs_role"
+  assume_role_policy = data.aws_iam_policy_document.logs_role.json
+}
+
+data "aws_iam_policy_document" "iam_policy_logs" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:DescribeLogGroups",
+      "logs:DescribeLogStreams",
+    ]
+
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "role_policy" {
+  name   = "role_policy"
+  role   = aws_iam_role.logs_role.id
+  policy = data.aws_iam_policy_document.iam_policy_logs.json
+}
+
